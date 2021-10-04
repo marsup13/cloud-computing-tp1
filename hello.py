596634b54c826d14b76ea45d16f71899d29942dc
bauuuu1021 <bboy010554@icloud.com>
2021-10-03 22:19:48 -0400

    Fix typo

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/') # Should be set to specific path, like '/cluster1' or 'cluster2'
+@app.route('/') # Should be set as a specific path, like '/cluster1' or '/cluster2'
 def my_app():
         return {
                 "instance": "cluster name",
