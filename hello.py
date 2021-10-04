547d63e493565b4f5d72923f57de7a5a4300e40e
bauuuu1021 <bboy010554@icloud.com>
2021-10-04 09:53:11 -0400

    Response public ip instead

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import request, jsonify, Flask
+from requests import get
 app = Flask(__name__)
 
-@app.route('/<path:text>', methods=['GET']) 
+@app.route('/<path:text>', methods=['GET']) # Should be set to specific path, like '/cluster1' or 'cluster2'
 def my_app(text):
     if text.startswith('cluster'):
         return {
-                "instance": "cluster name", # specify what should be return
+            "Public IP": get('https://api.ipify.org').content.decode('utf8'),
         }
-
